61021c4cea01d7df8cda548fc5f179a7a77743ca
Jérémi Panneton <jeremi.panneton@outlook.com>
2021-12-27 21:42:03 -0500

    Updates

---
+++ precache-manifest.67f515b4980be5a50785e17082056bcd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea0f3f893a1c8c23b33c",
+    "url": "/gifsmos-v/static/css/main.62dfe283.chunk.css"
+  },
+  {
+    "revision": "ea0f3f893a1c8c23b33c",
+    "url": "/gifsmos-v/static/js/main.ea0f3f89.chunk.js"
+  },
+  {
+    "revision": "80ca1bf7719e2a27e3cb",
+    "url": "/gifsmos-v/static/js/1.80ca1bf7.chunk.js"
+  },
+  {
+    "revision": "996a44a3c22580e37f60",
+    "url": "/gifsmos-v/static/js/runtime~main.996a44a3.js"
+  },
+  {
+    "revision": "fc4a46fd66483b10c5d23bb8e9e2590a",
+    "url": "/gifsmos-v/static/media/gifsmos-v-logo.fc4a46fd.svg"
+  },
+  {
+    "revision": "2edf57b67367ed625a72518bdeb0fb18",
+    "url": "/gifsmos-v/static/media/camera.2edf57b6.svg"
+  },
+  {
+    "revision": "261505625eac29af84d6aedd6ab73d13",
+    "url": "/gifsmos-v/static/media/preview.26150562.svg"
+  },
+  {
+    "revision": "ebf3b7c68ee8cd06ba46d90adc7d07e2",
+    "url": "/gifsmos-v/static/media/reset.ebf3b7c6.svg"
+  },
+  {
+    "revision": "6d8f24353e939170ff6ab17e3d5d05c9",
+    "url": "/gifsmos-v/static/media/burst.6d8f2435.svg"
+  },
+  {
+    "revision": "50e57a4a9c1821bafff979f8a3c9279a",
+    "url": "/gifsmos-v/static/media/saved.50e57a4a.svg"
+  },
+  {
+    "revision": "32c23130f0c13f49a17924b0be92a6f7",
+    "url": "/gifsmos-v/static/media/settings.32c23130.svg"
+  },
+  {
+    "revision": "3941b4c962d65fb0fc5106f5006d60a1",
+    "url": "/gifsmos-v/static/media/info.3941b4c9.svg"
+  },
+  {
+    "revision": "f35a3e98dd27afeef77be738e0408d27",
+    "url": "/gifsmos-v/static/media/left.f35a3e98.svg"
+  },
+  {
+    "revision": "1e9a60e1840b432b90a093956d9904f6",
+    "url": "/gifsmos-v/static/media/right.1e9a60e1.svg"
+  },
+  {
+    "revision": "ece18e4dc31d787fdbf2d5387fb9120d",
+    "url": "/gifsmos-v/index.html"
+  }
+];
